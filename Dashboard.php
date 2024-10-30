c730589b4915f5b2eddad3559fc9ad7adc0c7267
adriansyaha <adriansyaha210@gmail.com>
2024-10-30 14:11:52 +0700

    +titik merah notif (hidden)

--- Dashboard.php
+++ Dashboard.php
@@ -208,12 +208,12 @@
                     </div>
                     <div class="flex items-center lg:order-2">
                         <!-- INII Notifications -->
-                        <button type="button" id="notificationButton" onclick="loadPengaduan()" class="p-2 mr-2 text-gray-400 rounded-lg hover:text-yellow-400 hover:bg-gray-100">
-                        <span id="notificationDot" class="hidden absolute top-0 right-0 mt-1 mr-1 h-2 w-2 bg-red-500 rounded-full"></span>
-                            <span class="sr-only">View notifications</span>
+                        <button type="button" id="notificationButton" onclick="loadPengaduan()" class=" realtive p-2 mr-2 text-gray-400 rounded-lg hover:text-yellow-400 hover:bg-gray-100">
+                        <span id="notificationDot" class="hidden absolute top-8 right-15 h-2 w-2 bg-red-500 rounded-full transform translate-x-1/2 -translate-y-1/2"></span>  
+                        <span class="sr-only">View notifications</span>
                             <!-- Bell icon -->
-                            <svg class="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20"><path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z"/></svg>
-                            
+                            <svg class="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 20">
+                                <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z"/></svg>     
                         </button>
                         <!-- NOTIFIKASI CONTENT -->
                         <div id="notificationSidebar" class="fixed top-0 right-0 w-80 h-full bg-white shadow-lg transform translate-x-full transition-transform duration-300 overflow-y-auto">
