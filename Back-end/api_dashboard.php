e92781b45b2352122ffc79190dcfb382250f6329
reno <athayapm06@gmail.com>
2024-11-04 09:12:26 +0700

    reno donut

--- Back-end/api_dashboard.php
+++ Back-end/api_dashboard.php
@@ -1,17 +1,3 @@
 <?php
-    include 'config.php';
-    $db = new database();
 
-    // api aktivitas card
-    $pengaduan = mysqli_query($db->koneksi, "SELECT * FROM kejadian WHERE id_jenis_kejadian = 2");
-    $kehilangan = mysqli_query($db->koneksi, "SELECT * FROM kejadian WHERE id_jenis_kejadian = 1");
-    $rating = mysqli_query($db->koneksi, "SELECT * FROM kejadian WHERE id_jenis_kejadian = 3");
-
-    // api data statistik user
-    $mahasiswaCount = mysqli_query($db->koneksi, "SELECT * FROM user WHERE role = 3");
-    $dosenTendikCount = mysqli_query($db->koneksi, "SELECT * FROM user WHERE role = 2 and 4");
-    $unitLayananCount = mysqli_query($db->koneksi, "SELECT * FROM instansi");
-
-    // api data donat
-    
- 
+$conn = mysqli_connect("localhost", "root", "", "wicara");
