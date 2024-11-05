75714042f04c1665c62e7316abea6c74022b6564
Davin <dav1nalifianda@gmail.com>
2024-11-05 17:00:03 +0700

    api jenis pengaduan mobile

---
+++ api/api_jenisPengaduan.php
@@ -0,0 +1,42 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+include '../Back-end/config.php';
+$db = new database();
+
+// Fungsi untuk mengambil data mahasiswa
+function getJenisPengaduan($db) {
+    $query = "SELECT * FROM jenis_pengaduan";
+    $result = mysqli_query($db->koneksi, $query);
+    if ($result) {
+        $data = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+        return $data;
+    } else {
+        return [];
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $jenisPengaduan = getJenisPengaduan($db);
+    echo json_encode($jenisPengaduan);
+
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Ambil data dari JSON yang dikirimkan
+    $data = json_decode(file_get_contents("php://input"), true);
+    if (isset($data['id_jenis_pengaduan'])) {
+        $id_jenis_pengaduan = $data['id_jenis_pengaduan'];
+        echo json_encode($id_jenis_pengaduan);
+    } else {
+        echo json_encode(["success" => false, "message" => "ID jenis pengaduan tidak ditemukan."]);
+    }
+
+} else {
+    echo json_encode(["error" => "Invalid request method"]);
+}
+?>
