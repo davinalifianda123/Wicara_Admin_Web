7c95d0299304c3424080b3a6523644500ebd2beb
Fajar <maulanafr.27@gmail.com>
2024-11-06 08:17:14 +0700

    perbesar tanda X

--- mahasiswa.php
+++ mahasiswa.php
@@ -429,10 +429,14 @@
                     </form>
                     <div id="popup" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center hidden">
                     <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative">
-                        <button onclick="togglePopup()" class="absolute top-2 right-2 text-gray-400 hover:text-gray-600">
-                            &times;
-                        </button>
-                        <h3 class="text-lg font-semibold text-gray-700 mb-4">Tambah Mahasiswa</h3>
+                        <h3 class="flex items-center justify-between text-lg font-semibold text-gray-700 mb-4">
+                            Tambah Mahasiswa
+                            <button onclick="togglePopup()" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex items-center">
+                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                        </h3>
                         <form action="Back-end/simpan_tambah_mahasiswa.php" method="POST">
                             <div class="mb-4">
                                 <label class="block text-gray-700 text-sm font-bold mb-2">Nama</label>
