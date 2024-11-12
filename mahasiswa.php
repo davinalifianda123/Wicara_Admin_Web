be1410807d52291451ab22ba4ca964eedeafd5f6
Davin <dav1nalifianda@gmail.com>
2024-11-12 20:19:15 +0700

    mahasiswa delete sudah done

--- mahasiswa.php
+++ mahasiswa.php
@@ -544,7 +544,14 @@
                 <!-- Popup Form untuk Edit User -->
                 <div id="editPopup" class="fixed inset-0 z-50 hidden bg-gray-800 bg-opacity-50 flex items-center justify-center">
                     <div class="bg-white p-6 rounded-lg w-96">
-                        <h2 class="text-lg font-semibold mb-4">Edit User</h2>
+                        <div class="flex justify-between items-center mb-4">
+                            <h2 class="text-lg font-semibold">Edit User</h2>
+                            <button onclick="closePopup()" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex items-center">
+                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                        </div>
                         <form id="editForm" action="./Back-end/edit_mahasiswa.php" method="POST">
                             <input type="hidden" name="id_user" id="editUserId">
                             
@@ -580,8 +587,8 @@
                             </div>
 
                             <div class="flex justify-end">
-                                <button type="button" onclick="closePopup()" class="bg-gray-500 text-white px-4 py-2 rounded mr-2">Cancel</button>
-                                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
+                                <button type="submit" name="delete" value="1" class="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-red-600">Delete</button>
+                                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Save</button>
                             </div>
                         </form>
                     </div>
