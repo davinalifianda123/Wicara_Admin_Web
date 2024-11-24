e669d51a9cc2e1f52440d4b63f5211c03862bcd4
Ahmad <nolimitahmad123@gmail.com>
2024-11-24 17:18:56 +0700

    Anggota PIC #2

--- Back-end/config.php
+++ Back-end/config.php
@@ -233,14 +233,18 @@ if ($mysqli->connect_error) {
             return $hasil;
         }
 
-        function tampil_instansi_by_anggota($id_instansi)
+        function tampil_instansi_by_anggota()
         {
             $data = mysqli_query($this->koneksi, "SELECT a.*, b.*, c.* FROM anggota_instansi a
                                                                 INNER JOIN instansi b ON b.id_instansi = a.id_instansi
                                                                 INNER JOIN user c ON c.id_user = a.id_user
-                                                                WHERE id_instansi = '$id_instansi'");
-            $row = mysqli_fetch_array($data);
-            return $row;
+                                                                ");
+            $rows = [];
+            while ($row = $data->fetch_assoc()) {
+                $rows[] = $row;
+            }
+            return $rows;
+            
         }
 
         
