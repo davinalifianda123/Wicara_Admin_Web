f923e41e0df6e97f606b26ed43ed0c80024718ee
Davin <dav1nalifianda@gmail.com>
2024-12-01 11:34:58 +0700

    api_notif aman

--- api/api_notification.php
+++ api/api_notification.php
@@ -26,10 +26,14 @@ try {
     } else {
         // Jika tidak ada parameter ID, kembalikan daftar notifikasi
         $data = mysqli_query($database->koneksi, "
-            SELECT a.*, g.nama_kejadian, c.nama AS nama_user
+            SELECT a.*, g.nama_kejadian, c.nama AS nama_user, d.nama_status_kehilangan, e.nama_status_pengaduan, f.nama_jenis_pengaduan, h.nama_instansi
             FROM kejadian a
             LEFT JOIN jenis_kejadian g ON g.id_jenis_kejadian = a.id_jenis_kejadian
             INNER JOIN user c ON c.id_user = a.id_user
+            LEFT JOIN status_kehilangan d ON d.id_status_kehilangan = a.status_kehilangan
+            LEFT JOIN status_pengaduan e ON e.id_status_pengaduan = a.status_pengaduan
+            LEFT JOIN jenis_pengaduan f ON f.id_jenis_pengaduan = a.id_jenis_pengaduan
+            LEFT JOIN instansi h ON h.id_instansi = a.id_instansi
             ORDER BY a.tanggal DESC
         ");
 
@@ -57,10 +61,18 @@ try {
                 'title' => $row['judul'],
                 'category' => $row['nama_kejadian'],
                 'time' => timeAgo($row['tanggal']),
+                'tanggal' => $row['tanggal'],
                 'description' => $row['deskripsi'],
                 'rating' => (int)$row['skala_bintang'] ?? 0,
                 'status_notif' => (int)$row['status_notif'],
                 'nama_user' => $row['nama_user'] ?? 'Tidak ada nama pengguna',
+                'location' => $row['lokasi'] ?? 'Tidak ada lokasi',
+                'lampiran' => $row['lampiran'] ?? 'Tidak ada lampiran',
+                'jenis_barang' => $row['jenis_barang'] ?? 'Tidak ada jenis barang',
+                'status_kehilangan' => $row['nama_status_kehilangan'] ?? 'Tidak ada status kehilangan',
+                'status_pengaduan' => $row['nama_status_pengaduan'] ?? 'Tidak ada status pengaduan',
+                'jenis_pengaduan' => $row['nama_jenis_pengaduan'] ?? 'Tidak ada jenis pengaduan',
+                'instansi' => $row['nama_instansi'] ?? 'Tidak ada instansi'
             ];
         }
 
