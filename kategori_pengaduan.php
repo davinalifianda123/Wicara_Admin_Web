d10f2330b9630945ab3ca7bd2c0425d3b1bc145a
khildasa <khildasalsabilaa@gmail.com>
2024-12-04 09:28:51 +0700

    Modal Popup Versi Beta

--- kategori_pengaduan.php
+++ kategori_pengaduan.php
@@ -497,8 +497,7 @@
                 </ul>
             </nav>
 
-
-            <!--Edit Delete MODAL-->
+            <!--MODAL Detail Edit Hapus-->
             <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden fixed inset-0 z-50 items-center justify-center bg-black bg-opacity-50">
                 <div class="relative p-4 w-full max-w-md max-h-full">
                     <div class="relative bg-white rounded-lg shadow">
@@ -508,7 +507,6 @@
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                 </svg>
-                                <span class="sr-only">Close modal</span>
                             </button>
                         </div>
                         <form class="p-4 md:p-5" action="" method="POST">
@@ -516,7 +514,6 @@
                                 <div class="col-span-2">
                                     <label for="modal-id" class="block mb-2 text-sm font-medium text-gray-900">ID</label>
                                     <input type="text" id="modal-id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" disabled>
-                                    <input type="hidden" name="id_jenis_pengaduan" id="hidden-id" value="<?php echo $x['id_jenis_pengaduan']; ?>">
                                 </div>
                                 <div class="col-span-2">
                                     <label for="modal-nama" class="block mb-2 text-sm font-medium text-gray-900">Nama Kategori</label>
@@ -525,14 +522,55 @@
                             </div>
                             <div class="flex justify-end mt-4">
                                 <button type="submit" name="action" value="save" class="text-white inline-flex items-center bg-[#34C759] hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-1">Simpan</button>
-                                <button type="submit" name="action" value="delete" class="text-white inline-flex items-center bg-[#F12626] hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-1" onclick="return confirm('Apakah Anda yakin ingin menghapus kategori ini?');">Hapus</button>
+                                <button class="text-white inline-flex items-center bg-[#F12626] hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-1" type="button" onclick="openConfirmModal()">Hapus</button>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
 
-            <!-- Add New MODAL -->
+            <!--MODAL Konfirmasi Hapus-->
+            <div id="confirm-delete-modal" tabindex="-1" aria-hidden="true" class="hidden fixed inset-0 z-50 items-center justify-center bg-black bg-opacity-50">
+                <div class="relative p-4 w-full max-w-md max-h-full">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        </button>
+                        <div class="p-4 md:p-5 text-center">
+                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                                <div id="confirm-delete-modal">
+                                    <h3 id="confirm-delete-text" class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                    </h3>
+                                </div>
+                          <button type="submit" name="action" value="delete" class="text-white inline-flex items-center bg-[#F12626] hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-1" onclick="">Hapus</button>
+                          <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onclick="closeEditModal()">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- MODAL Sukses Hapus -->
+            <div id="successModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+                    <!-- Modal content -->
+                    <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                        <button type="button" class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="successModal">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                        <div class="w-12 h-12 rounded-full bg-green-100 dark:bg-green-900 p-2 flex items-center justify-center mx-auto mb-3.5">
+                            <svg aria-hidden="true" class="w-8 h-8 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Success</span>
+                        </div>
+                        <p class="mb-4 text-lg font-semibold text-gray-900 dark:text-white">Successfully removed product.</p>
+                        <button data-modal-toggle="successModal" type="button" class="py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-primary-900">
+                            Continue
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- MODAL Tambah Kategori Baru -->
             <div id="add-modal" tabindex="-1" aria-hidden="true" class="hidden fixed inset-0 z-50 items-center justify-center bg-black bg-opacity-50">
                 <div class="relative p-4 w-full max-w-md max-h-full">
                     <div class="relative bg-white rounded-lg shadow">
@@ -616,12 +654,28 @@
                 }
                 reader.readAsDataURL(event.target.files[0]);
             }
+            
+            function openConfirmModal() {
+                const namaKategori = document.getElementById('modal-nama').value;
+                document.getElementById('confirm-delete-text').textContent = 
+                    `Apakah yakin ingin menghapus kategori "${namaKategori}"?`;
+                
+                    closeEditModal();
+                document.getElementById('confirm-delete-modal').classList.remove('hidden');
+                document.getElementById('confirm-delete-modal').classList.add('flex');
+            }
+
+
+              function closeConfirmModal() {
+                  document.getElementById('confirm-delete-modal').classList.remove('flex');
+                  document.getElementById('confirm-delete-modal').classList.add('hidden');
+              }
+
 
             function openEditModal(button) {
               const id = button.getAttribute('data-id');
               const nama = button.getAttribute('data-nama');
               document.getElementById('modal-id').value = id;
-              document.getElementById('hidden-id').value = id;
               document.getElementById('modal-nama').value = nama;
               document.getElementById('crud-modal').classList.remove('hidden');
               document.getElementById("crud-modal").classList.add("flex");
